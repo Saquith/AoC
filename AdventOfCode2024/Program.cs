06732ed948ebb5088bfa3f6ff5bc57f34d35db6d
Roeland Krijgsman <roeland.krijgsman@pggm.nl>
2024-12-05 11:48:37 +0100

    Initial day one challenge commit

---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode2024.challenges;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// Setup
+var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+var config = builder.Build();
+
+var servicesCollection = new ServiceCollection()
+    // .AddSingleton<Service?>()
+    .AddSingleton<IChallenge, Challenge1>()
+    .AddSingleton<IConfiguration>(config);
+
+var serviceProvider = servicesCollection.BuildServiceProvider();
+
+// Run
+using var scope = serviceProvider.CreateScope();
+var challenge = scope.ServiceProvider.GetService<IChallenge>();
+await challenge!.ReadInput();
+
+var result = await challenge.Calculate();
+Console.WriteLine($"Result to current challenge:\r\n{result}");
+
+Console.WriteLine("Press enter to exit..");
+Console.ReadLine();
---
+++ AdventOfCode2024/challenges/IChallenge.cs
@@ -0,0 +1,7 @@
+﻿namespace AdventOfCode2024.challenges;
+
+public interface IChallenge
+{
+    Task ReadInput();
+    Task<string> Calculate();
+}
