683459df533ef8d0bd12dee4876d9bb608c88ca4
Roeland Krijgsman <roelandkrijgsman@gmail.com>
2024-12-07 14:15:15 +0100

    Add first part of challenge 3 with regex group

---
+++ AdventOfCode2024/Models/3/Operation.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2024.Models._3;
+
+public class Operation(string type, long first, long second)
+{
+    public string OperationType { get; set; } = type;
+    public long FirstNumber { get; set; } = first;
+    public long SecondNumber { get; set; } = second;
+}
